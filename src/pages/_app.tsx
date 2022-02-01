781a2213909b5fe789231f59821d4d5405613294
Pedroabreu155 <pedroliveirabreu@gmail.com>
2022-02-01 17:24:20 -0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { GlobalStyle } from 'styles/global';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate Nextjs</title>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" href="/images/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="Project starter to work with Nextjs and Styled-Components"
+        />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
